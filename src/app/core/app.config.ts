69a7227d01f2a5a89f12e21c5b54d8730d20cfdc
Thiago Leite <thiagofmleite@gmail.com>
2018-07-23 23:19:38 -0300

    fixed request

--- src/app/core/app.config.ts
+++ src/app/core/app.config.ts
@@ -5,9 +5,4 @@ export const config = {
     PLANET_RULE: /^https:\/\/swapi.co\/api\/planets\/\d+\/$/,
     STARSHIP_RULE: /^https:\/\/swapi.co\/api\/starships\/\d+\/$/,
     VEHICLE_RULE: /^https:\/\/swapi.co\/api\/vehicles\/\d+\/$/,
-    PIXABAY_API: 'https://pixabay.com/api/',
-    PIXABAY_KEY: '9631899-d99dd1ed924a9b304718c503f',
-    GOOGLE_API: 'https://www.googleapis.com/customsearch/v1',
-    GOOGLE_CX: '010645083362894859360:rjfqvda2d6s',
-    GOOGLE_KEY: 'AIzaSyBc1RXNn-uV0OKpEypmZzjEONsFvue1mAI'
 }
