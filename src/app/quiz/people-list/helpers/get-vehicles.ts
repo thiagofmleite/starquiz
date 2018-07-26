2b9902f01537343de17cb1ca5172820c5b3bbf08
Thiago Leite <thiagofmleite@gmail.com>
2018-07-26 02:02:08 -0300

    Resolvido problema com load dos veículos

--- src/app/quiz/people-list/helpers/get-vehicles.ts
+++ src/app/quiz/people-list/helpers/get-vehicles.ts
@@ -5,7 +5,9 @@ export function getVehiclesFromPeople(people: People, service: VehicleService) {
     const requests$ = people.vehicles.map(api => service.getFromApi(api));
     people.vehicles = []
     requests$.map(req => req.subscribe(
-        response => people.vehicles.push(response.name),
+        response => {
+            people.vehicles.push(response.name)
+        },
         error => console.log(error)
     ));
 } 
